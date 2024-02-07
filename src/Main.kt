f1bb69edd0592fa3ab9abc52413b97e80eeab096
nancy wainaina <wainainanancywambui@gmail.com>
2024-02-07 23:59:19 +0300

    Variables

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var fullNames = "Marion Shiomwa"
+    println("fullNames")
+    var age = "17"
+    println ("age")
+    var phoneNumber = ("0790059397")
+    println ("phoneNumber")
+    var weight = ("65kg")
+    println ("weight")
+    var citizen = ("kenyan")
+    println ("citizen")
+
+}
